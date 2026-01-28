54a7587d6333f0e9cf26cb0a387a0483480036f4
Jaden Majid <majid.jaden@gmail.com>
2026-01-27 21:36:45 -0800

    added to discriminator model

--- src/discrim_model.py
+++ src/discrim_model.py
@@ -1,2 +1,26 @@
 import torch
+import torch.nn as nn
 
+"""
+DiscriminatorBlock 
+https://arxiv.org/pdf/1609.04802 
+"""
+class DiscriminatorBlock(nn.Module):
+    def __init__(self, in_channels=64,out_channels=64, stride=2):
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.a = nn.LeakyReLU(inplace=True)
+
+    def forward(self, x):
+        return self.a(self.bn(self.conv(x)))
+
+"""
+DiscriminatorModel 
+https://arxiv.org/pdf/1609.04802 
+"""
+class DiscriminatorModel(nn.Module):
+    def __init__(self):
+        raise NotImplementedError
+    
+    def forward(self, x):
+        raise NotImplementedError
--- src/gen_model.py
+++ src/gen_model.py
@@ -5,15 +5,16 @@ import torch.nn as nn
 
 color_channels = 3
 """
-BottleneckResidualBlock
+BottleneckResidualBlock 
+https://arxiv.org/pdf/1609.04802 
 """
 class BottleneckResidualBlock(nn.Module):
     def __init__(self, in_channels=64,out_channels=64):
-        self.conv0 = nn.Conv2d(in_channels, out_channels, kernel_size=3, bias=False)
+        self.conv0 = nn.Conv2d(in_channels, out_channels, kernel_size=3,padding=1, bias=False)
         self.bn0 = nn.BatchNorm2d(color_channels)
         self.a0 = nn.PReLU()
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, bias=False)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(color_channels)
         self.a1 = nn.PReLU()
 
@@ -26,10 +27,11 @@ class BottleneckResidualBlock(nn.Module):
 
 """
 UpscalerBlock
+https://arxiv.org/pdf/1609.04802
 """
 class UpscalerBlock(nn.Module):
     def __init__(self, in_channels=256,out_channels=256):
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, bias=False)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3,padding=1, bias=False)
         self.ps = nn.PixelShuffle(2)
         self.a = nn.PReLU()
 
@@ -38,6 +40,7 @@ class UpscalerBlock(nn.Module):
 
 """
 GenerativeModel
+https://arxiv.org/pdf/1609.04802
 """
 class GenerativeModel(nn.module):
     def __init__(self, num_blocks=5, block_channels=64):
@@ -47,17 +50,17 @@ class GenerativeModel(nn.module):
 
         self.blocks = nn.Sequential(*[BottleneckResidualBlock(block_channels, block_channels) for _ in range(num_blocks)])
         self.after_blocks = nn.Sequential(
-            nn.Conv2d(block_channels, 4*block_channels, kernel_size=3),
+            nn.Conv2d(block_channels, 4*block_channels, kernel_size=3,padding=1,),
             nn.BatchNorm2d(color_channels)
         )
         self.upscaler = UpscalerBlock()
-        self.final_conv = nn.Conv2d(4*block_channels, 3, kernel_size=9)
+        self.final_conv = nn.Conv2d(4*block_channels, 3, kernel_size=9,padding=4)
     
     def forward(self, x):
         input_image = x
         x = self.inputa(self.input_conv(x))
         x = self.blocks(x)
-        x = self.after_blocks(x)
+        x = self.after_blocks(x) + input_image
         x = self.upscaler(x)
         x = self.final_conv(x)
         return x
